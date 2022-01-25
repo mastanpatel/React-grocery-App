bccde09ec63ab4ba3b75aca40827c11b31f5e14b
Mastan Patel <m.mastanpatel@gmail.com>
2022-01-25 16:11:38 +0530

    addded the edit css

--- src/List.js
+++ src/List.js
@@ -2,8 +2,14 @@ import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
 const List = ({ items }) => {
   return (
-    <div>
-      <p>{items}</p>
+    <div className='grocery-list'>
+      <article className='grocery-item'>
+        <p className='title'>{items}</p>
+        <div className='btn-container'>
+          <button className='edit-btn'><FaEdit /></button>
+          <button className='delete-btn'><FaTrash /></button>
+        </div>
+      </article>
     </div>
   )
 }
